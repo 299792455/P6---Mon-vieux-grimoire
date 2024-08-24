1183ee08647c60ed6ddfd3d17ec3878c566785fc
Christophe Pires <iglesias.pires@gmail.com>
2024-08-24 15:46:52 +0200

    maj app.js : rajout route app.use setheader + début route POST

--- backend/app.js
+++ backend/app.js
@@ -6,11 +6,27 @@ const Book = require ('./models/books.js');
 
 app.use(cors());
 
+app.use(express.json())
+
 mongoose.connect('mongodb+srv://75017pi:T5BYZOWTegrmGu9O@cluster6.gahlt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster6',
     { useNewUrlParser: true,
       useUnifiedTopology: true })
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+    app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+      next();
+    });
+
+    app.post('/api/books', (req, res, next) => {
+        console.log(req.body);
+        res.status(201).json({
+          message:"ok usa c'est créé" //test pr route post pour plus tard (à changer)
+        })
+    });
 
     app.get('/api/books', (req, res, next) => {
         Book.find()
